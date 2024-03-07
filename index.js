73f977f059203ac45014a1fc3b210986d39a83e6
Gabriel D'Ostuni <gabdostuni47400@gmail.com>
2024-03-07 11:11:51 +0100

    feat: delete files folders

--- index.js
+++ index.js
@@ -5,33 +5,86 @@ function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
+function parse_to_delete(to_delete) {
+  const data = to_delete.split("\n").filter((d) => d != "");
+
+  const files = [];
+
+  if (data.length === 0)
+    throw new Error(
+      "No files were defined, please ensure you enter destinations to upload your files!"
+    );
+
+  data.forEach((upload) => {
+    const [root, name] = upload.trim().split("=>");
+
+    if (!root || !name) return;
+
+    files.push({
+      root: root.trim(),
+      files: [name.trim()],
+    });
+  });
+
+  return files;
+}
+
 async function start() {
   const server = core.getInput("server");
   const api_key = core.getInput("api_key");
   const server_id = core.getInput("server_id");
+
   const power = toString(core.getInput("power"));
+  const deletes = parse_to_delete(core.getInput("power"));
 
   try {
-    await axios
-      .request({
-        method: "post",
-        maxBodyLength: Infinity,
-        url: `https://${server}/api/client/servers/${server_id}/power`,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${api_key}`,
-        },
-        data: JSON.stringify({
-          signal: power,
-        }),
-      })
-      .then(async () => {
-        console.log("Action on server : " + power + " ...");
-        await delay(5000);
-      })
-      .catch((error) => {
-        core.setFailed(error);
-      });
+    if (power) {
+      await axios
+        .request({
+          method: "post",
+          maxBodyLength: Infinity,
+          url: `https://${server}/api/client/servers/${server_id}/power`,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${api_key}`,
+          },
+          data: JSON.stringify({
+            signal: power,
+          }),
+        })
+        .then(async () => {
+          console.log("Power action on server : " + power + " ...");
+          await delay(5000);
+        })
+        .catch((error) => {
+          core.setFailed(error);
+        });
+    }
+
+    if (deletes) {
+      for (const to_delete of deletes) {
+        await axios
+          .request({
+            method: "post",
+            maxBodyLength: Infinity,
+            url: `https://${server}/api/client/servers/${server_id}/power`,
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${api_key}`,
+            },
+            data: JSON.stringify(to_delete),
+          })
+          .then(async () => {
+            console.log(
+              "Deletes file/folder on server : " + to_delete.root + " " + to_delete.files[0] +  " ... "
+            );
+            await delay(5000);
+          })
+          .catch((error) => {
+            core.setFailed(error);
+          });
+      }
+    }
   } catch (error) {
     core.setFailed(error);
   }
